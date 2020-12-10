283f3da0a7396be2cf6c76f259034ec9943dbb11
Kelsie Merchant <eislek02@gmail.com>
2020-12-10 15:34:28 -0600

    Refactor to while loop instead of recursion to avoid 'RecursionError: maximum recursion depth exceeded while calling a Python object'

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -29,50 +29,35 @@ def canUnlockAll(boxes):
         if type(box) is not list:
             return False
     # creates a copy of boxes to not affect the original list of lists
-    unlockedBoxes = deepcopy(boxes)
+    copyBoxes = deepcopy(boxes)
     # calls unlockBox() method to open box 0
     # unlockBox() will recursively open any boxes with keys from the opened box
-    unlockedBoxes = unlockBox(unlockedBoxes, 0)
+    keys_list = [0]
+    while len(keys_list) > 0:
+        key = keys_list[0]
+        keys_list = keys_list[1:]
+        if type(key) is not int or key < 0:
+            # if key is invalid, move to next valid key
+            continue
+        # mark that the box has been opened by appeneding a -1 flag
+        # since all keys are pos ints, -1 will not be mistaken for a valid key
+        copyBoxes[key].append(-1)
+        # loop through any new keys found in the recently opened box
+        # save new keys to keys_list to potentially open more boxes
+        for new_key in copyBoxes[key]:
+            if new_key >= len(copyBoxes):
+                # if new key is out of range of the available boxes, continue
+                continue
+            if -1 in copyBoxes[new_key]:
+                # if the box has previously been opened, continue
+                continue
+            # update the list of availble key
+            keys_list.append(new_key)
     # after opening all possible boxes, check that total boxes have been opened
     # opened boxes are noted with a -1 flag
-    for box in unlockedBoxes:
+    for box in copyBoxes:
         if -1 not in box:
             # if a box is missing the -1 flag, it was not unlocked
             return False
     # returns true if all boxes in the previous loop were indicated as unlocked
     return True
-
-
-def unlockBox(boxes, key):
-    """
-    unlocks the box corresponding to a given key, recursively
-
-    parameters:
-        boxes (list): list of list representing the boxes and any keys inside
-        key (int): the index of the box to unlock
-
-    returns:
-        boxes, with marker (-1) indicating that the box has been opened
-    """
-    if type(key) is not int or key < 0:
-        # if key is invalid
-        return boxes
-    # mark that the box has been opened by appeneding a -1 flag
-    # since all keys are positive ints, -1 will not be mistaken for a valid key
-    boxes[key].append(-1)
-    # loop through any new keys found in the recently opened box
-    # use new keys to potentially open more boxes with the unlockBox() method
-    for new_key in boxes[key]:
-        if new_key is -1:
-            # if new key is the flag for an opened box, continue
-            continue
-        if new_key >= len(boxes):
-            # if new key is out of range of the available boxes, continue
-            continue
-        if -1 in boxes[new_key]:
-            # if the box has previously been opened, continue
-            continue
-        # update the list of boxes by calling unlockBox() to open new boxes
-        boxes = unlockBox(boxes, new_key)
-    # after unlocking all boxes with the available keys, return boxes
-    return boxes
