4f6e33c6b64765df9c2ad0bdac18a81be5f525f6
Kelsie Merchant <eislek02@gmail.com>
2020-12-14 17:03:20 -0600

    Add given source code for header file and functions needed to test output

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a new number as a node into sorted list
+ * @head: pointer to the head of the sorted singly linked list
+ * @number: number to sort and add into list as new node
+ * Return: pointer to the new node, or NULL if error encountered
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
