8727a039648ca5cee1b5bb27e1e5f2241c621f34
Kelsie Merchant <eislek02@gmail.com>
2020-12-14 22:57:02 -0600

    Initial commit of C function to insert into sorted singly linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -9,5 +9,36 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
+	listint_t *new = NULL, *mover = NULL;
 
+	/* check if not given pointer to list */
+	if (head == NULL)
+		return (NULL);
+	/* set new as a stand alone node with number as data, next to NULL */
+	new = malloc(sizeof(listint_t *));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+	/* if orginally given empty list, set head to new node */
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	/* else, set mover to head of list to move until ready to insert */
+	mover = *head;
+	/* check if need to insert at beginning of sorted list */
+	if (mover->n > number)
+	{
+		*head = new;
+		new->next = mover;
+		return (new);
+	}
+	/* move until at location to insert new node */
+	while (mover->next != NULL && mover->next->n <= number)
+		mover = mover->next;
+	new->next = mover->next;
+	mover->next = new;
+	return (new);
 }
