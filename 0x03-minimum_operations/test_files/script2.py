1a85f9947ab27eafaeb7889a99e0ff5e912a27cf
Kelsie Merchant <eislek02@gmail.com>
2021-01-12 02:08:54 -0600

    Add test files to test efficiency between checking every odd number and strictly checking only prime numbers

--- 0x03-minimum_operations/test_files/script2.py
+++ 0x03-minimum_operations/test_files/script2.py
@@ -8,27 +8,29 @@ minOpsStrict = __import__('0-minoperations-strict_primes').minOperations
 n = 0
 favor1 = 0
 favor2 = 0
-while n < 10:
-    random_num = randint(0, 9999999)
+while n < 500:
+    random_num = randint(0, 999999)
     start1 = time()
     result1 = minOperations(random_num)
     stop1 = time()
+    print("n = " + str(random_num), end="\t-->\t")
     start2 = time()
     result2 = minOpsStrict(random_num)
     stop2 = time()
     if result1 != result2:
         print('we found a fail!')
-        print("n = " + str(random_num), end="\t-->\t")
         print("\nresult1: ", result1, "\tresult2: ", result2)
         break
+    print(result1)
     time_diff1 = stop1 - start1
     time_diff2 = stop2 - start2
+    print("time difference = ", time_diff1 - time_diff2)
     if time_diff1 - time_diff2 < 0:
         favor1 += 1
     elif time_diff1 - time_diff2 > 0:
         favor2 += 1
-    if (n % 2) == 0:
-        print(n, "\tfavor1: ", favor1, "\tfavor2: ", favor2)
+    if (n % 10) == 0:
+        print("favor1: ", favor1, "\tfavor2: ", favor2)
     n += 1
 if (favor1 == favor2):
     print("1 and 2 are even")
