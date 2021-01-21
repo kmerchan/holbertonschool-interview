a7d3af9fcc3b7396d2e12018dfa4557bb2eb6f74
Kelsie Merchant <eislek02@gmail.com>
2021-01-21 01:25:06 -0600

    Update test file with more test cases

--- 0x05-linked_list_palindrome/test_files/0-main.c
+++ 0x05-linked_list_palindrome/test_files/0-main.c
@@ -9,27 +9,66 @@
  */
 int main(void)
 {
-    listint_t *head;
+	listint_t *head, *odd;
 
-    head = NULL;
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 1);
-    print_listint(head);
+	head = NULL;
+	printf("Testing head is NULL\n");
+	if (is_palindrome(NULL) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+	printf("Testing *head is NULL\n");
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+	add_nodeint_end(&head, 1);
+	printf("testing one element\n");
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+	add_nodeint_end(&head, 17);
+	printf("testing 2 elements\n");
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
 
-    if (is_palindrome(&head) == 1)
-        printf("Linked list is a palindrome\n");
-    else
-        printf("Linked list is not a palindrome\n");
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
 
-    free_listint(head);
+	free_listint(head);
 
-    return (0);
+	odd = NULL;
+	add_nodeint_end(&odd, 1);
+	add_nodeint_end(&odd, 17);
+	add_nodeint_end(&odd, 972);
+	add_nodeint_end(&odd, 50);
+	add_nodeint_end(&odd, 98);
+	add_nodeint_end(&odd, 50);
+	add_nodeint_end(&odd, 972);
+	add_nodeint_end(&odd, 17);
+	add_nodeint_end(&odd, 1);
+	print_listint(odd);
+
+	if (is_palindrome(&odd) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(odd);
+
+	return (0);
 }
