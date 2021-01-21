a544f0f73b920db0d1816b9086845fe82f4005ee
Kelsie Merchant <eislek02@gmail.com>
2021-01-21 01:24:17 -0600

    Update header file with comments and new helper function prototype

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -1,6 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/* INCLUDED LIBRARIES */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* STRUCTS AND DEFINITIONS */
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,10 +21,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* FUNCTION PROTOTYPES */
+/* Given function to print the list in order */
 size_t print_listint(const listint_t *h);
+/* Given function to add a node to the end of the list */
 listint_t *add_nodeint_end(listint_t **head, const int n);
+/* Given function to free a list */
 void free_listint(listint_t *head);
 
+/* Function to check if list is a palindrome */
 int is_palindrome(listint_t **head);
+/* Subfunction to calculate the length of the list */
+int list_length(listint_t *head);
 
 #endif /* LISTS_H */
