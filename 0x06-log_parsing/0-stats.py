be205a15d2ec6d076ec96222a1d9602c21820d3b
Kelsie Merchant <eislek02@gmail.com>
2021-01-28 23:48:46 -0600

    Use try/except block instead of signal handler to catch KeyboardInterrupt

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,8 +3,7 @@
 Reads stdin line by line and computes metrics
 """
 
-from sys import exit, stdin
-from signal import signal, SIGINT
+from sys import stdin
 
 if __name__ == "__main__":
     total_size = 0
@@ -14,30 +13,27 @@ if __name__ == "__main__":
     for status in list_status_codes:
         status_codes[status] = 0
     count = 0
-    for line in stdin:
-        args = line.split(" ")
-        status_codes[args[7]] += 1
-        if args[8][-1] == '\n':
-            args[8][:-1]
-        total_size += int(args[8])
-        if count % 10 == 0 and count != 0:
+    try:
+        for line in stdin:
+            args = line.split(" ")
+            status_codes[args[7]] += 1
+            if args[8][-1] == '\n':
+                args[8][:-1]
+                total_size += int(args[8])
+                if count % 10 == 0:
+                    print("File size: {}".format(total_size))
+                    for status in sorted(status_codes.keys()):
+                        if status_codes[status] != 0:
+                            print("{}: {}".format(status, status_codes[status]))
+            count += 1
+        if count % 10 != 0:
             print("File size: {}".format(total_size))
             for status in sorted(status_codes.keys()):
                 if status_codes[status] != 0:
                     print("{}: {}".format(status, status_codes[status]))
-        count += 1
-    if count % 10 != 0:
+    except KeyboardInterrupt as err:
         print("File size: {}".format(total_size))
-        for status in status_codes.sorted():
+        for status in sorted(status_codes.keys()):
             if status_codes[status] != 0:
                 print("{}: {}".format(status, status_codes[status]))
-
-# def signal_handler(sig, frame):
-    # """
-    # catches signal CTRL-C and prints stats
-    # """
-    # print("File size: {}".format(total_size))
-    # for status in status_codes.sorted():
-        # if status_codes[status] != 0:
-            # print("{}: {}".format(status, status_codes[status]))
-    # exit(0)
+        raise
