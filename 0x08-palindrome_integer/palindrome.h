07527fcc8e5d33f97bdbf7a13ddb418355c0395b
Kelsie Merchant <eislek02@gmail.com>
2021-02-11 15:25:55 -0600

    Add new helper function prototype

--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -8,6 +8,8 @@
 /* FUNCTION PROTOTYPES */
 /* function to check if unsigned long int is a palindrome */
 int is_palindrome(unsigned long n);
+/* helper function to check if unsigned int is a palindrome recursively */
+int palindrome_check(unsigned long *n, int *digit);
 /* helper function to calculate place as power of 10 based on digit count */
 unsigned long power_of_ten(int digit);
 
