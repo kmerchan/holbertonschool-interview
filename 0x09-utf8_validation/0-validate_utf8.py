65f3a977e59d7be86d64c980d6dfc931aa6df702
Kelsie Merchant <eislek02@gmail.com>
2021-02-17 14:46:36 -0600

    Initial commit of function to determine if data set represents valid UTF-8 encoding

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines a method that determines if a given data set
+represents valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents valid UTF-8 encoding
+    """
+    return False
