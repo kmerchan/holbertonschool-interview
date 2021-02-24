8e16ede953e95e70469cefc40fc9065988d6e5df
Kelsie Merchant <eislek02@gmail.com>
2021-02-24 00:00:28 -0600

    Refactor code to reduce lines in function to pass Betty style guidelines

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -16,60 +16,39 @@ int slide_line(int *line, size_t size, int direction)
 {
 	int index = 0, placeholder = 0;
 
-	if (size <= 1)
+	if (size < 1 || (direction != SLIDE_RIGHT && direction != SLIDE_LEFT))
 		return (0);
 	if (direction == SLIDE_RIGHT)
 	{
 		placeholder = (size - 1);
 		for (index = (size - 2); index >= 0; index--)
 		{
-			if (line[index] == 0)
-				continue;
-			if (line[index] == line[placeholder])
+			if (line[index] == line[placeholder] && line[index])
+				slide(line, index, placeholder--);
+			else if (line[index] != line[placeholder] && line[index])
 			{
-				slide(line, index, placeholder);
-				placeholder--;
-			}
-			else if (line[index] != line[placeholder])
-			{
+				if (line[placeholder] != 0)
+					placeholder--;
 				if (line[placeholder] == 0)
 					slide(line, index, placeholder);
-				else
-				{
-					placeholder--;
-					if (line[placeholder] == 0)
-						slide(line, index, placeholder);
-				}
 			}
 		}
 	}
 	else if (direction == SLIDE_LEFT)
 	{
-		placeholder = 0;
 		for (index = 1; index < (int)size; index++)
 		{
-			if (line[index] == 0)
-				continue;
-			if (line[index] == line[placeholder])
+			if (line[index] == line[placeholder] && line[index])
+				slide(line, index, placeholder++);
+			else if (line[index] != line[placeholder] && line[index])
 			{
-				slide(line, index, placeholder);
-				placeholder++;
-			}
-			else if (line[index] != line[placeholder])
-			{
+				if (line[placeholder] != 0)
+					placeholder++;
 				if (line[placeholder] == 0)
 					slide(line, index, placeholder);
-				else
-				{
-					placeholder++;
-					if (line[placeholder] == 0)
-						slide(line, index, placeholder);
-				}
 			}
 		}
 	}
-	else
-		return (0);
 	return (1);
 }
 
