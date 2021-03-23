90836005d37528307d1551bf260442d998471e87
Kelsie Merchant <eislek02@gmail.com>
2021-03-22 23:32:29 -0500

    Initial commit of function to build AVL tree from sorted array

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - builds an AVL tree from sorted array without rotation
+ *
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of elements in the array
+ *
+ * Return: pointer to the root node of the created AVL tree, or NULL if failed
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+
+	return (NULL);
+}
