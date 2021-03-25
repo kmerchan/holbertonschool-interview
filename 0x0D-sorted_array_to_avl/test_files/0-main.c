4fef04a5f70f20c85bde2d25ab7d1077bf05c858
Kelsie Merchant <eislek02@gmail.com>
2021-03-25 00:26:12 -0500

    Fix tabs to format test file with C styling

--- 0x0D-sorted_array_to_avl/test_files/0-main.c
+++ 0x0D-sorted_array_to_avl/test_files/0-main.c
@@ -10,11 +10,11 @@
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; ++i)
-        printf("(%03d)", array[i]);
-    printf("\n");
+	for (i = 0; i < size; ++i)
+		printf("(%03d)", array[i]);
+	printf("\n");
 }
 
 /**
@@ -42,60 +42,60 @@ void binary_tree_delete(binary_tree_t *tree)
  */
 int main(void)
 {
-    avl_t *tree;
-    int array1[] = {
-        1, 2, 20, 21, 22, 32, 34, 47, 62, 68,
-        79, 84, 87, 91, 95, 98
-    };
-    size_t n1 = sizeof(array1) / sizeof(array1[0]);
-    int array2[] = {
-        1, 2, 20, 21, 22, 32, 34, 47, 62, 68,
-        79, 84, 87
-    };
-    size_t n2 = sizeof(array2) / sizeof(array2[0]);
+	avl_t *tree;
+	int array1[] = {
+		1, 2, 20, 21, 22, 32, 34, 47, 62, 68,
+		79, 84, 87, 91, 95, 98
+	};
+	size_t n1 = sizeof(array1) / sizeof(array1[0]);
+	int array2[] = {
+		1, 2, 20, 21, 22, 32, 34, 47, 62, 68,
+		79, 84, 87
+	};
+	size_t n2 = sizeof(array2) / sizeof(array2[0]);
 
-    printf("Even-sized array:\n");
-    tree = sorted_array_to_avl(array1, n1);
-    if (!tree)
-    {
-	    printf("Returned NULL\n");
-	    return (1);
-    }
-    print_array(array1, n1);
-    binary_tree_print(tree);
-    binary_tree_delete(tree);
-    tree = NULL;
+	printf("Even-sized array:\n");
+	tree = sorted_array_to_avl(array1, n1);
+	if (!tree)
+	{
+		printf("Returned NULL\n");
+		return (1);
+	}
+	print_array(array1, n1);
+	binary_tree_print(tree);
+	binary_tree_delete(tree);
+	tree = NULL;
 
-    printf("Odd-sized array:\n");
-    tree = sorted_array_to_avl(array2, n2);
-    if (!tree)
-    {
-	    printf("Returned NULL\n");
-	    return (1);
-    }
-    print_array(array2, n2);
-    binary_tree_print(tree);
-    binary_tree_delete(tree);
-    tree = NULL;
+	printf("Odd-sized array:\n");
+	tree = sorted_array_to_avl(array2, n2);
+	if (!tree)
+	{
+		printf("Returned NULL\n");
+		return (1);
+	}
+	print_array(array2, n2);
+	binary_tree_print(tree);
+	binary_tree_delete(tree);
+	tree = NULL;
 
-    printf("Incorrect size:\n");
-    tree = sorted_array_to_avl(array2, 0);
-    if (!tree)
-	    printf("Correctly returned NULL\n");
-    else
-    {
-	    printf("Didn't return NULL\n");
-	    return (1);
-    }
+	printf("Incorrect size:\n");
+	tree = sorted_array_to_avl(array2, 0);
+	if (!tree)
+		printf("Correctly returned NULL\n");
+	else
+	{
+		printf("Didn't return NULL\n");
+		return (1);
+	}
 
-    printf("NULL array:\n");
-    tree = sorted_array_to_avl((int *)NULL, n1);
-    if (!tree)
-	    printf("Correctly returned NULL\n");
-    else
-    {
-	    printf("Didn't return NULL\n");
-	    return (1);
-    }
-    return (0);
+	printf("NULL array:\n");
+	tree = sorted_array_to_avl((int *)NULL, n1);
+	if (!tree)
+		printf("Correctly returned NULL\n");
+	else
+	{
+		printf("Didn't return NULL\n");
+		return (1);
+	}
+	return (0);
 }
