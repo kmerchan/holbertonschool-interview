2bd8a227f1dcd9d968fb09879cbc251e9a5d6c86
Kelsie Merchant <eislek02@gmail.com>
2021-04-02 23:42:07 -0500

    Add test file used by checker for condition of value not in the list, but greater than later index, not included in express lane

---
+++ 0x0E-linear_skip/test_files/0-checker_main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "search.h"
+
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	skiplist_t *head, *res;
+	int array[23] = {
+		0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 53, 61, 62, 100, 100, 100, 111, 122, 123, 145, 155, 260
+	};
+
+	head = create_skiplist(array, 23);
+	print_skiplist(head);
+	res = linear_skip(head, 123456789);
+	printf("Found at index: %p\n", (void *) res);
+	free_skiplist(head);
+	return (0);
+}
