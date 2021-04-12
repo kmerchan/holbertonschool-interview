15179effc60db7019cc005eda3e7c510f20cbe6f
Kelsie Merchant <eislek02@gmail.com>
2021-04-11 23:09:40 -0500

    Remove function and use async with promise and await to get all characters list before printing individual character names; updated to print in order after conversations with peers

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,26 +3,26 @@
 const request = require('request');
 const myArgs = process.argv.splice(2);
 const URL = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
-function charactersFunc (character, i, stop) {
-  if (i === stop) {
-    return;
-  }
-  const movieCharacter = character[i];
-  request(movieCharacter, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(JSON.parse(body).name);
-    }
-  });
-  i++;
-  charactersFunc(character, i, stop);
-}
-request(URL, function (err, response, body) {
+request.get(URL, async (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
     const character = JSON.parse(body).characters;
-    charactersFunc(character, 0, character.length);
+    const characterList = characterURLs => {
+      const promise = new Promise((resolve, reject) => {
+        request.get(characterURLs, (err, response, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(body);
+          }
+        });
+      });
+      return promise;
+    };
+    for (let i = 0; i < character.length; i++) {
+      const result = await characterList(character[i]);
+      console.log(JSON.parse(result).name);
+    }
   }
 });
