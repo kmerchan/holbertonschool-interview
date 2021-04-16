929694e932b9b3d7e48cb10fcd30627abb2ea0a8
Kelsie Merchant <eislek02@gmail.com>
2021-04-16 02:41:17 -0500

    Add new test conditions and add expected output to print statements for easy comparison

--- 0x10-rain/test_files/0-main.py
+++ 0x10-rain/test_files/0-main.py
@@ -6,4 +6,10 @@ rain = __import__('0-rain').rain
 
 if __name__ == "__main__":
     walls = [0, 1, 0, 2, 0, 3, 0, 4]
-    print(rain(walls))
+    print(6, rain(walls))
+    walls = []
+    print(0, rain(walls))
+    walls = [4, 0, 3, 0, 2, 0, 1, 0]
+    print(6, rain(walls))
+    walls = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    print(6, rain(walls))
