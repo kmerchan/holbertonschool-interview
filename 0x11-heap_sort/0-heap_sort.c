7a03c48839209be63ce0d088b604730108ce09b8
Kelsie Merchant <eislek02@gmail.com>
2021-04-22 22:13:13 -0500

    Initial commit

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * heap_sort - sorts an array of ints in ascending order
+ * using Heap Sort algorithm
+ *
+ * @array: the array of ints to sort
+ * @size: the size of the array to sort
+ *
+ */
+void heap_sort(int *array, size_t size)
+{
+	(void)size;
+
+	return (array);
+}
