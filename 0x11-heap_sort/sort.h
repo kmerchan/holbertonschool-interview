ea6792e25888b8dcd246b82cacef4bd916036df7
Kelsie Merchant <eislek02@gmail.com>
2021-04-22 22:02:09 -0500

    Add header file with function prototypes

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* INCLUDED LIBRARIES */
+#include stdio.h
+#include stdlib.h
+
+/* FUNCTION PROTOTYPES */
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+
+#endif
