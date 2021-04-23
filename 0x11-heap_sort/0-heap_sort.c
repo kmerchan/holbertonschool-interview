ec0f1f6d23774c64e2b82737d8ff89ba35fc84ea
Kelsie Merchant <eislek02@gmail.com>
2021-04-22 23:41:24 -0500

    Update functions to pass parameters for full array and full size to be used for printing

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -10,21 +10,38 @@
  */
 void heap_sort(int *array, size_t size)
 {
-	int i = 0, temp = 0;
-
 	if (array == NULL || size <= 1)
 		return;
+	heap_sort_print(array, size, array, size);
+}
+
+/**
+ * heap_sort_print - heap sort with full array as parameter for printing
+ *
+ * @array: array to sort
+ * @size: size of array to sort
+ * @array_p: array to print
+ * @size_p: size of array to print
+ *
+ */
+void heap_sort_print(int *array, size_t size, int *array_p, size_t size_p)
+{
+	int i = 0, temp = 0;
+
 	for (i = (size - 1) / 2; i >= 0; i--)
 	{
-		heapify(array, size, i);
+		heapify(array, size, i, array_p, size_p);
 	}
 	for (i = (size - 1); i > 0; i--)
 	{
 		temp = array[i];
 		array[i] = array[0];
 		array[0] = temp;
+		array_p[i] = array[i];
+		array_p[0] = array[0];
+		print_array(array_p, size_p);
 
-		heapify(array, i, 0);
+		heapify(array, i, 0, array_p, size_p);
 	}
 }
 
@@ -34,9 +51,11 @@ void heap_sort(int *array, size_t size)
  * @array: the array of ints to sort
  * @size: the size of the array to sort
  * @i: current index in array
+ * @array_p: array to print
+ * @size_p: size of array to print
  *
  */
-void heapify(int *array, size_t size, int i)
+void heapify(int *array, size_t size, int i, int *array_p, size_t size_p)
 {
 	int max = i;
 	int left_child = 2 * i + 1;
@@ -52,8 +71,10 @@ void heapify(int *array, size_t size, int i)
 		temp = array[i];
 		array[i] = array[max];
 		array[max] = temp;
-		print_array(array, size);
+		array_p[i] = array[i];
+		array_p[max] = array[max];
+		print_array(array_p, size_p);
 
-		heapify(array, size, max);
+		heapify(array, size, max, array_p, size_p);
 	}
 }
