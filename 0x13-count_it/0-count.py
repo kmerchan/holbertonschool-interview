dfeeafb1130b94ae54581271687df0eceee9dde9
Kelsie Merchant <eislek02@gmail.com>
2021-05-06 22:23:15 -0500

    Reaarange code to handle recursive call of function before trying to sort and print

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -42,36 +42,35 @@ def count_words(subreddit, word_list, after=None, count={}):
             word = word.lower()
             count[word] += title.count(word)
     after = data.get("after")
-    if after is None:
-        result = []
-        for k in count.keys():
-            if count[k] != 0:
-                if result == []:
-                    result.append("{}: {}".format(k, count[k]))
-                else:
-                    for i in range(len(result)):
-                        if count[k] > int(result[i].split(' ')[1]):
-                            result = result[:i] + \
-                                     ["{}: {}".format(k, count[k])] + \
-                                     result[i:]
-                            break
-                        elif count[k] == int(result[i].split(' ')[1]):
-                            alpha_list = [k, result[i].split(' ')[0]]
-                            j = 1
-                            while count[k] == int(result[i + j].split(' ')[1]):
-                                alpha_list.append(result[i + j].split(' ')[0])
-                            alpha_list = alpha_list.sort
-                            for j in range(len(alpha_list)):
-                                if k == alpha_list[j]:
-                                    result = result[:i + j] + \
-                                             ["{}: {}".format(k, count[k])] + \
-                                             result[i + j:]
-                        else:
-                            continue
+    if after is not None:
+        return (count_words(subreddit, word_list, after, count))
+    result = []
+    for k in count.keys():
+        if count[k] != 0:
+            if result == []:
+                result.append("{}: {}".format(k, count[k]))
+            else:
+                for i in range(len(result)):
+                    if count[k] > int(result[i].split(' ')[1]):
+                        result = result[:i] + \
+                            ["{}: {}".format(k, count[k])] + \
+                            result[i:]
+                        break
+                    elif count[k] == int(result[i].split(' ')[1]):
+                        alpha_list = [k, result[i].split(' ')[0]]
+                        j = 1
+                        while count[k] == int(result[i + j].split(' ')[1]):
+                            alpha_list.append(result[i + j].split(' ')[0])
+                        alpha_list = alpha_list.sort
+                        for j in range(len(alpha_list)):
+                            if k == alpha_list[j]:
+                                result = result[:i + j] + \
+                                    ["{}: {}".format(k, count[k])] + \
+                                    result[i + j:]
                     else:
-                        result.append("{}: {}".format(k, count[k]))
-        if result != []:
-            for printing in result:
-                print(printing)
-        return
-    return (count_words(subreddit, word_list, after, count))
+                        continue
+                else:
+                    result.append("{}: {}".format(k, count[k]))
+    if result != []:
+        for printing in result:
+            print(printing)
