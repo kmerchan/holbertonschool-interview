78136dc78831bc74941d7244ef142df2b3a0c514
Kelsie Merchant <eislek@gmail.com>
2021-06-07 14:28:34 +0000

    Initial commit of function to implement merge sort algorithm

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * merge_sort - sorts an array of ints using top-down merge sort algorithm
+ * @array: array of integers to sort
+ * @size: size of the array of integers to sort
+ *
+ */
+
+void merge_sort(int *array, size_t size)
+{
+
+}
