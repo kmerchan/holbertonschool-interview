8a65693973f4449ab25ad1e26364edbd4a96dd78
Kelsie Merchant <eislek02@gmail.com>
2021-06-07 18:47:15 -0500

    Add function to implement merge sort algorithm to sort an array of ints

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -10,14 +10,62 @@
 void merge_sort(int *array, size_t size)
 {
 	int mid = size / 2;
-	int left = 0, right = 0;
 
-	if (size == 0 || array == NULL)
+	if (size <= 1 || array == NULL)
 		return;
-	if (size <= 2)
-	{
-		print_array(array);
-	}
 	merge_sort(array, mid);
-	merge_sort(array, size - mid);
+	merge_sort(&array[mid], size - mid);
+	merge(array, mid, size);
+}
+
+
+/**
+ * merge - merges two subarrays together
+ * @array: array to merge
+ * @mid: index of mid-point
+ * @size: size of array to merge
+ *
+ */
+
+void merge(int *array, int mid, size_t size)
+{
+	int *holder = malloc(sizeof(int) * size);
+	int left = 0, right = mid, index = 0;
+
+	if (holder == NULL)
+		return;
+	printf("Merging...\n[left]: ");
+	print_array(array, mid);
+	printf("[right]: ");
+	print_array(&array[mid], size - mid);
+	while (left < mid && right < (int)size)
+	{
+		if (array[left] <= array[right])
+		{
+			holder[index] = array[left];
+			left++;
+		}
+		else
+		{
+			holder[index] = array[right];
+			right++;
+		}
+		index++;
+	}
+	while (left < mid)
+	{
+		holder[index] = array[left];
+		left++;
+		index++;
+	}
+	while (right < (int)size)
+	{
+		holder[index] = array[right];
+		right++;
+		index++;
+	}
+	for (index = 0; index < (int)size; index++)
+		array[index] = holder[index];
+	printf("[Done]: ");
+	print_array(array, size);
 }
