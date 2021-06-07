7d93535387e28e09990a481a15dd8b996011741b
Kelsie Merchant <eislek@gmail.com>
2021-06-07 14:23:28 +0000

    Add header file with function prototypes and included libraries

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,14 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* INCLUDED LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* FUNCTION PROTOTYPES */
+/* Given function to print array */
+void print_array(const int *array, size_t size);
+/* Function to implement top-down merge sort algorithm */
+void merge_sort(int *array, size_t size);
+
+#endif
