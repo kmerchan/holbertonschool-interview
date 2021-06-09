29e96dbef83c41dbbb1ec608b63d5dd234492782
Kelsie Merchant <eislek02@gmail.com>
2021-06-09 13:13:10 -0500

    Update function prototypes based on refactoring in 0-merge_sort.c

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -10,6 +10,7 @@
 void print_array(const int *array, size_t size);
 /* Function to implement top-down merge sort algorithm */
 void merge_sort(int *array, size_t size);
+void merge_sort_holder(int *array, size_t size, int *holder);
 void merge(int *holder, int *array, int mid, size_t size);
 
 #endif
