8633da7613d78769093061891c2a6780b28a7522
Kelsie Merchant <eislek02@gmail.com>
2021-06-09 13:02:49 -0500

    Update merge function prototype to include holder integer array

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -10,6 +10,6 @@
 void print_array(const int *array, size_t size);
 /* Function to implement top-down merge sort algorithm */
 void merge_sort(int *array, size_t size);
-void merge(int *array, int mid, size_t size);
+void merge(int *holder, int *array, int mid, size_t size);
 
 #endif
