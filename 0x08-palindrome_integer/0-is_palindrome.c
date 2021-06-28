30141d3ca0938715b2e20862dd53b458cb2a301d
Kelsie Merchant <eislek02@gmail.com>
2021-06-27 22:24:46 -0500

    Add comments to show how the algorithm works

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -14,11 +14,17 @@ int is_palindrome(unsigned long n)
 
 	while (n != 0)
 	{
+		/* find the value of the last digit */
 		last = n % 10;
+		/* store the value in a reverse number */
+		/* multiply by ten to increase the digit count of reverse */
 		reverse *= 10;
+		/* add the last digit to reverse */
 		reverse += last;
+		/* cut off the last digit so the new last digit is next digit */
 		n /= 10;
 	}
+	/* check if original number and calculated reverse are the same */
 	if (original == reverse)
 		return (1);
 	return (0);
