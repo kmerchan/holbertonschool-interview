1f36e7361d4531a62008923c969ef3e2941d8cb9
Kelsie Merchant <eislek02@gmail.com>
2021-06-27 19:17:34 -0500

    Add comments to clearly show how the code functions

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -12,11 +12,14 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
+	/* Allocate memory for new node and check if NULL */
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
+	/* Assign value and parent with given values */
 	new_node->n = value;
 	new_node->parent = parent;
+	/* Set left and right children to NULL */
 	new_node->left = NULL;
 	new_node->right = NULL;
 	return (new_node);
