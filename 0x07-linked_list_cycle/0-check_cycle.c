9c2fb4ea3f05477f1bae81b685b0e1b2fdea48f8
Kelsie Merchant <eislek02@gmail.com>
2021-06-27 22:18:59 -0500

    Add comments to show how the algorithm works

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -11,18 +11,27 @@ int check_cycle(listint_t *list)
 {
 	listint_t *tortoise = NULL, *hare = NULL;
 
+	/* if list is NULL or empty, return 0 for no cycle */
 	if (list == NULL || list->next == NULL)
 		return (0);
+	/* set slow moving pointer to start of list */
 	tortoise = list;
+	/* set fast moving pointer to the next node */
 	hare = list->next;
+	/* ensure hare can move forward two spots without segfaulting */
 	while (hare && hare->next)
 	{
+		/* if both pointers ever point to same node, a cycle is found */
 		if (tortoise == hare)
 			return (1);
+		/* otherwise, slow pointer moves forward one node */
 		tortoise = tortoise->next;
+		/* fast pointer jumps forward two nodes */
 		hare = hare->next->next;
 	}
+	/* if pointers match and are not NULL, indicate cycle */
 	if (hare && tortoise == hare)
 		return (1);
+	/* return 0 if made to end of the list and did not find a match */
 	return (0);
 }
