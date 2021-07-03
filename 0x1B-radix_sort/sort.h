caca1bf7bda4b55923be793335b54a34d03e2100
Kelsie Merchant <eislek02@gmail.com>
2021-07-02 23:32:09 -0500

    Add header file with included libraries and function prototypes

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,18 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* INCLUDED LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* FUNCTION PROTOTYPES */
+/* given function to print an array of ints */
+void print_array(const int *array, size_t size);
+/* function to sort array using radix sort */
+void radix_sort(int *array, size_t size);
+/* helper function to determine max number of digits in array of ints */
+int digit_counting(int *array, size_t size);
+/* helper function to get the divisor to get specific digit from number */
+int get_divisor(int current_digit);
+
+#endif
