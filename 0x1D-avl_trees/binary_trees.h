577095af43d0ad40a913f84852069d9d53ce7a1c
Kelsie Merchant <eislek02@gmail.com>
2021-07-21 22:44:53 -0500

    Add additional function prototypes for helper functions to determine if tree is valid AVL tree

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -4,6 +4,7 @@
 /* INCLUDED LIBRARIES */
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /* STRUCTS AND DEFINITIONS */
 /**
@@ -16,10 +17,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 /* typedef for binary tree struct */
 typedef struct binary_tree_s binary_tree_t;
@@ -31,5 +32,13 @@ typedef struct binary_tree_s avl_t;
 void binary_tree_print(const binary_tree_t *tree);
 /* function that checks if binary tree is a valid AVL tree */
 int binary_tree_is_avl(const binary_tree_t *tree);
+/* function to check if tree is valid binary search tree at every level */
+int is_bst_recursive(const binary_tree_t *tree, int min, int max);
+/* function to check if each subtree is balanced using preorder traversal */
+int is_balanced_preorder(const binary_tree_t *tree);
+/* function to determine if the tree is balanced */
+int is_balanced(const binary_tree_t *tree);
+/* helper function to get height of subtrees, including root node */
+size_t height_helper(const binary_tree_t *tree);
 
 #endif
