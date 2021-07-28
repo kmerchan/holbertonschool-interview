440a24052b3b966fb37c80aef9769787ad034e38
Kelsie Merchant <eislek02@gmail.com>
2021-07-27 22:07:27 -0500

    Add header file with included libraries and function prototype

---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* INCLUDED LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* FUNCTION PROTOTYPES */
+int wildcmp(char *s1, char *s2);
+
+#endif
