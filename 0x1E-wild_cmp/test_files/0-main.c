a014f5d52bdc0db21e95214d7539aaa4908c2847
Kelsie Merchant <eislek02@gmail.com>
2021-07-28 00:13:17 -0500

    Add optional print statements to turn on to more clearly see which strings are being compared

--- 0x1E-wild_cmp/test_files/0-main.c
+++ 0x1E-wild_cmp/test_files/0-main.c
@@ -10,28 +10,40 @@ int main(void)
 {
 	int r;
 
+	/* printf("%s vs %s:\n", "main.c", "*.c"); */
 	r = wildcmp("main.c", "*.c");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main.c", "m*a*i*n*.*c*"); */
 	r = wildcmp("main.c", "m*a*i*n*.*c*");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main.c", "main.c"); */
 	r = wildcmp("main.c", "main.c");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main.c", "m*c"); */
 	r = wildcmp("main.c", "m*c");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main.c", "ma********************************c"); */
 	r = wildcmp("main.c", "ma********************************c");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main.c", "*"); */
 	r = wildcmp("main.c", "*");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main.c", "***"); */
 	r = wildcmp("main.c", "***");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main.c", "m.*c"); */
 	r = wildcmp("main.c", "m.*c");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main.c", "**.*c"); */
 	r = wildcmp("main.c", "**.*c");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main-main.c", "ma*in.c"); */
 	r = wildcmp("main-main.c", "ma*in.c");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "main", "main*d"); */
 	r = wildcmp("main", "main*d");
 	printf("%d\n", r);
+	/* printf("%s vs %s:\n", "abc", "*b"); */
 	r = wildcmp("abc", "*b");
 	printf("%d\n", r);
 	return (0);
