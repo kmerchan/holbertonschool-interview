29e96813bc5d0a7793bb81337ab8b3f283d93370
Kelsie Merchant <eislek02@gmail.com>
2021-08-17 22:13:11 -0500

    Add header file with included libraries and function prototype

---
+++ 0x21-substring/substring.h
@@ -0,0 +1,11 @@
+#ifndef SUBSTRING_H
+#define SUBSTRING_H
+
+/* INCLUDED LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* FUNCTION PROTOTYPES */
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif
